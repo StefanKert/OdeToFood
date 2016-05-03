7824216b05efa0e03ea2999cea8415c7b5abfb00
Stefan Kert <stefan.kert@inode.at>
2016-05-03 06:48:12 +0200

    Initiall commit

---
+++ src/OdeToFood/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OdeToFood.Services;
+
+namespace OdeToFood
+{
+    public class Startup
+    {
+        public Startup() {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+
+        private IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddSingleton(provider => Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
+        }
+
+        public void Configure(IApplicationBuilder app, IGreeter greeter) {
+            app.UseIISPlatformHandler();
+
+            app.Run(async (context) => {
+                await context.Response.WriteAsync(greeter.GetGreeting());
+            });
+        }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
